a3fcc3ac7162f86147eba34c3934aa59d8c572b1
Wsieslaw <wsieslaw.mackiewicz@gmail.com>
2025-05-09 16:59:50 +0200

    Feat () Run spring aplication trough cmd

---
+++ 01-Spring-Boot-Overview/05-command-line-demo/TestTwo/src/test/java/com/example/TestTwo/TestTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class TestTwoApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTwoApplicationTests.class, args);
+	}
+
+}
