5039f329c928c87172c96d7bfbf2d7387911e1fe
Wsieslaw <wsieslaw.mackiewicz@gmail.com>
2025-05-09 22:29:51 +0200

    Feat (Properties) Discovering more properties

--- 01-Spring-Boot-Overview/06-properties-demo/TestTwo/src/main/java/com/example/TestTwo/rest/GoldenRestController.java
+++ 01-Spring-Boot-Overview/06-properties-demo/TestTwo/src/main/java/com/example/TestTwo/rest/GoldenRestController.java
@@ -7,8 +7,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GoldenRestController {
+
+    // inject properties values
+
+    @Value("${nothingThere.class}")
+    private String nothingThereLevel;
+
+    @Value("${coolApp.name}")
+    private String myAppName;
+
+
     @GetMapping("/")
     public String nothingThere() {
         return "Goodbye";
+    }
+
+    @GetMapping("/getInfo")
+    public String getInfo() {
+        return "Nothing There Level :" + nothingThereLevel + "\n My App Name :" + myAppName;
     }
 }
