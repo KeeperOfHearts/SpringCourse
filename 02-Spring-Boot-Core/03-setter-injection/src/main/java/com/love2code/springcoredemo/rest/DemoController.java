88600468fe838ff6540fcac6c19c5e45f90d34a3
Wsieslaw <wsieslaw.mackiewicz@gmail.com>
2025-05-17 22:08:38 +0200

    feat (setter injection) Autowired setter method

---
+++ 02-Spring-Boot-Core/03-setter-injection/src/main/java/com/love2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package com.love2code.springcoredemo.rest;
+
+import com.love2code.util.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    private Coach myCoach;
+
+    @Autowired
+    public void setCoach(Coach coach) {
+        myCoach = coach;
+    }
+//    public  DemoController(Coach coach) {
+//        myCoach = coach;
+//    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
