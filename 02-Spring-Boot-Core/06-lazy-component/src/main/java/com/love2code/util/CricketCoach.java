02d54df7f2bb0828c76a1832454c131add4cbce1
Wsieslaw <wsieslaw.mackiewicz@gmail.com>
2025-05-21 21:02:17 +0200

    feat (lazy) lazy-initialization

---
+++ 02-Spring-Boot-Core/06-lazy-component/src/main/java/com/love2code/util/CricketCoach.java
@@ -0,0 +1,17 @@
+package com.love2code.util;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+
+    public CricketCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes";
+    }
+}
