29296c9948eaf8047853aec2806cf93584b3c27e
Wsieslaw <wsieslaw.mackiewicz@gmail.com>
2025-06-06 18:44:22 +0200

    feat (Querying Objects Part 1) add method to find all

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2distort/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2distort/cruddemo/dao/StudentDAOImpl.java
@@ -3,9 +3,13 @@ package com.luv2distort.cruddemo.dao;
 import com.luv2distort.cruddemo.entity.Student;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 
 @Repository
@@ -31,5 +35,12 @@ public class StudentDAOImpl implements StudentDAO {
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+
+        return theQuery.getResultList();
     }
 }
