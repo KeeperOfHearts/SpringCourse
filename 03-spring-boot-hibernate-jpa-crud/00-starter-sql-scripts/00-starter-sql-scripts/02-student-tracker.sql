9416879f5a3ac3171a82f6655d2028edbc296a5c
Wsieslaw <wsieslaw.mackiewicz@gmail.com>
2025-06-14 18:13:42 +0200

    feat (spring-rest-crud) initial commit

--- 03-spring-boot-hibernate-jpa-crud/00-starter-sql-scripts/00-starter-sql-scripts/02-student-tracker.sql
+++ 03-spring-boot-hibernate-jpa-crud/00-starter-sql-scripts/00-starter-sql-scripts/02-student-tracker.sql
@@ -1,17 +1,15 @@
-CREATE DATABASE  IF NOT EXISTS `student_tracker`;
-USE `student_tracker`;
+CREATE DATABASE  IF NOT EXISTS `quotesOfTheDay`;
+USE `quotesOfTheDay`;
 
 --
 -- Table structure for table `student`
 --
 
-DROP TABLE IF EXISTS `student`;
+DROP TABLE IF EXISTS `quotes`;
 
 CREATE TABLE `student` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `first_name`varchar(45) DEFAULT NULL,
-  `last_name` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
+  `quote`varchar(120) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
