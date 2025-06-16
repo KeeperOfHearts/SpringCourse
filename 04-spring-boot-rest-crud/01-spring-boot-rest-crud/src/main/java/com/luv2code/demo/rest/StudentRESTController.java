6ec88ca84fdc0a110366915e045403462fcb0b37
Wsieslaw <wsieslaw.mackiewicz@gmail.com>
2025-06-16 11:38:59 +0200

    refactor (StudentRESTContoller) remove unnecessary list shenanigans

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRESTController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRESTController.java
@@ -1,6 +1,7 @@
 package com.luv2code.demo.rest;
 
 import com.luv2code.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,15 +13,28 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRESTController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> theStudents = new ArrayList<>();
+    private List<Student> theStudents ;
+
+
+    @PostConstruct
+    private void loadData()    {
+        // create the students
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("John", "Doe"));
         theStudents.add(new Student("Roman", "Zui"));
         theStudents.add(new Student("Wilson", "Smith"));
+    }
 
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return theStudents;
+    }
+
+    @GetMapping("/students/{id}")
+    public Student getStudent(int id) {
+        return theStudents.get(id);
     }
 
 }
