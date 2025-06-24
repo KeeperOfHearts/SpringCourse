d435d1f254f342070a05d4007f2409fddfdffe8e
Wsieslaw <wsieslaw.mackiewicz@gmail.com>
2025-06-24 19:32:00 +0200

    feat (Employee DAO) DAO, Entity and RestContoller implement

---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/luv2code/cruddemo/rest/EmployeeRESTContoller.java
@@ -0,0 +1,26 @@
+package com.luv2code.cruddemo.rest;
+
+
+import com.luv2code.cruddemo.dao.EmployeeDAO;
+import com.luv2code.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRESTContoller {
+    private EmployeeDAO employeeDAO;
+
+    public EmployeeRESTContoller(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+}
