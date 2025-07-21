469c007a09753b197d52eb782312326f533eefb0
My Name <wsieslaw.mackiewicz@gmail.com>
2025-07-21 19:34:29 +0200

    feat (Spring MVC Validation - Required Fields)

---
+++ 07-spring-boot-thymeleaf/validation-demo/src/main/java/com/luv2code/springdemo/mvc/controller/CustomerController.java
@@ -0,0 +1,37 @@
+package com.luv2code.springdemo.mvc.controller;
+
+
+import com.luv2code.springdemo.mvc.entity.Customer;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.naming.Binding;
+
+@Controller
+public class CustomerController {
+
+    @GetMapping("/")
+    public String showForm(Model model) {
+
+        model.addAttribute("customer", new Customer());
+
+        return "customer-form";// why no work
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(
+            @Valid @ModelAttribute("customer") Customer customer,
+            BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "customer-form";
+        }
+
+        return "customer-confirmation";
+    }
+}
