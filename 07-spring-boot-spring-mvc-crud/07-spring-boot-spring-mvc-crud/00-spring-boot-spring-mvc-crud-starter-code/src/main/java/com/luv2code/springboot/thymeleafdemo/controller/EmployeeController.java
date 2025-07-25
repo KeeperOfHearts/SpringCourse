67a9322faf8d89acc199b4c6f8adb7bb98057cd3
My Name <wsieslaw.mackiewicz@gmail.com>
2025-07-25 20:34:34 +0200

    add&feat (Set up & Basic Thymelead CRUD - Read)

---
+++ 07-spring-boot-spring-mvc-crud/07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model) {
+        // get employees from the service
+        model.addAttribute("employees", employeeService.findAll());
+
+        return "list-employees";
+    }
+}
