ffe0a1639a46db75467dbc65393fcdc0d8c8bb33
My Name <wsieslaw.mackiewicz@gmail.com>
2025-08-09 16:19:34 +0200

    feat (Main App && Save Instructor)

--- 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,8 @@
 package com.luv2code.cruddemo;
 
+import com.luv2code.cruddemo.dao.AppDAO;
+import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +16,27 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 		return runner -> {
-			System.out.println("Bye, World!");
+			createInstructor(appDAO);
 		};
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+//		Instructor instructor = new Instructor("Chad" , "Darby", "darby@luv2code.com");
+//		InstructorDetail instructorDetail = new InstructorDetail(
+//				"https://www.luv2code.com/youtube", "Luv 2 Code!!!"
+//		);
+
+		Instructor instructor = new Instructor("Madhu" , "Patek", "madhu@luv2code.com");
+		InstructorDetail instructorDetail = new InstructorDetail(
+				"https://www.luv2code.com/youtube", "Guitar"
+		);
+
+		instructor.setInstructorDetail(instructorDetail);
+
+		System.out.println("Saving instructor: " + instructor);
+		appDAO.save(instructor);
 	}
 
 }
