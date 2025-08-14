bf6448d471313c4b248e679e99cfc18f925b0e6a
My Name <wsieslaw.mackiewicz@gmail.com>
2025-08-14 15:33:28 +0200

    feat (Create Course Entity)

---
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -0,0 +1,83 @@
+package com.luv2code.cruddemo;
+
+import com.luv2code.cruddemo.dao.AppDAO;
+import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO) {
+		return runner -> {
+//			createInstructor(appDAO);
+
+//			Instructor instructor = findInstructor(appDAO);
+
+//		deleteInstructor(appDAO);
+
+//		findInstructorDetail(appDAO);
+
+		deleteInstructorDetail(appDAO);
+		};
+	}
+
+	private void createInstructor(AppDAO appDAO) {
+//		Instructor instructor = new Instructor("Max" , "Danno", "danno@luv2code.com");
+//		InstructorDetail instructorDetail = new InstructorDetail(
+//				"https://www.luv2code.com/youtube", "Luv 2 Code!!!"
+//		);
+//		instructor.setInstructorDetail(instructorDetail);
+//
+//		System.out.println("Saving instructor: " + instructor);
+//		appDAO.save(instructor);
+
+	}
+
+	private InstructorDetail findInstructorDetail(AppDAO appDAO) {
+		int id = 2;
+		System.out.println("Finding instructor detail with id: " + id);
+		InstructorDetail instructorDetail = appDAO.findInstructorDetailById(id);
+		if (instructorDetail != null) {
+			System.out.println("Found instructor detail: " + instructorDetail);
+			System.out.println("Associated instructor: " + instructorDetail.getInstructor());
+		} else {
+			System.out.println("Instructor detail with id " + id + " not found.");
+		}
+		return instructorDetail;
+	}
+
+	private void deleteInstructorDetail(AppDAO appDAO) {
+		int id = 6;
+		System.out.println("Deleting instructor detail with id: " + id);
+		appDAO.deleteInstructorDetailById(id);
+	}
+
+	private Instructor findInstructor(AppDAO appDAO) {
+		int id = 2;
+		System.out.println("Finding instructor with id: " + id);
+		Instructor instructor = appDAO.findById(id);
+		if (instructor != null) {
+			System.out.println("Found instructor: " + instructor);
+		} else {
+			System.out.println("Instructor with id " + id + " not found.");
+		}
+		return instructor;
+	}
+
+	private void deleteInstructor(AppDAO appDAO) {
+		int id = 4;
+		System.out.println("Deleting instructor with id: " + id);
+		appDAO.deleteById(id);
+		System.out.println("Deleted instructor with id: " + id);
+	}
+
+}
