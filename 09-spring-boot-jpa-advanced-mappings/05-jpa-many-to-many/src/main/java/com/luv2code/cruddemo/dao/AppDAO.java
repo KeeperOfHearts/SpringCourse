9b6dc4d361254ed74450d60b0c298abd140c6a83
My Name <wsieslaw.mackiewicz@gmail.com>
2025-08-25 14:17:17 +0200

    setup (Many to Many - Setup)

---
+++ 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/com/luv2code/cruddemo/dao/AppDAO.java
@@ -0,0 +1,36 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Course;
+import com.luv2code.cruddemo.entity.Instructor;
+import com.luv2code.cruddemo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+    void save(Instructor instructor);
+
+    Instructor findById(int id);
+
+    void deleteById(int id);
+
+    InstructorDetail findInstructorDetailById(int id);
+
+    void deleteInstructorDetailById(int id);
+
+    List<Course> findCoursesByInstructorId(int instructorId);
+
+    Instructor findInstructorByIdJoinFetch(int id);
+
+    void update(Instructor instructor);
+
+    void update(Course course);
+
+    Course findCourseById(int id);
+
+    void deleteCourseById(int id);
+
+    void save(Course course);
+
+    Course findCourseAndReviewsById(int id);
+
+}
