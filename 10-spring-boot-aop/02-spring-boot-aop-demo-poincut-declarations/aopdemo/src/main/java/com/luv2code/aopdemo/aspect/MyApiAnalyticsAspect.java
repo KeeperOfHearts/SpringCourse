888ac7ca07d17f40142167de95cd1e963b2f471d
My Name <wsieslaw.mackiewicz@gmail.com>
2025-09-02 17:31:56 +0200

    feat (Ordering Aspects)

--- 10-spring-boot-aop/02-spring-boot-aop-demo-poincut-declarations/aopdemo/src/main/java/com/luv2code/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-poincut-declarations/aopdemo/src/main/java/com/luv2code/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -1,22 +1,17 @@
 package com.luv2code.aopdemo.aspect;
 
+
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Order(2)
-public class MyDemoLoggingAspect {
-
-
-//    @Before("execution(public void add*())")
-
-
+@Order(1)
+public class MyApiAnalyticsAspect {
     @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n=====>>> Executing @Before advice on everything)\n");
+    public void performApiAnalytics() {
+        System.out.println("\n=====>>> Performing API analytics");
     }
 }
--- 10-spring-boot-aop/02-spring-boot-aop-demo-poincut-declarations/aopdemo/src/main/java/com/luv2code/aopdemo/aspect/MyCloudLogAsyncAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-poincut-declarations/aopdemo/src/main/java/com/luv2code/aopdemo/aspect/MyCloudLogAsyncAspect.java
@@ -2,21 +2,15 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Order(2)
-public class MyDemoLoggingAspect {
-
-
-//    @Before("execution(public void add*())")
-
-
+@Order(3)
+public class MyCloudLogAsyncAspect {
     @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n=====>>> Executing @Before advice on everything)\n");
+    public void logToCloudAsync() {
+        System.out.println("\n=====>>> Logging to cloud in async fashion");
     }
 }
