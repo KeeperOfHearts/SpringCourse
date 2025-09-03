86d703bc199ee9afd75bac2cf11c3fa5a5b8f506
My Name <wsieslaw.mackiewicz@gmail.com>
2025-09-03 18:13:49 +0200

    feat (JoinPoints Arguments)

--- 10-spring-boot-aop/02-spring-boot-aop-demo-poincut-declarations/aopdemo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-poincut-declarations/aopdemo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,5 +1,6 @@
 package com.luv2code.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -16,7 +17,24 @@ public class MyDemoLoggingAspect {
 
 
     @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
         System.out.println("\n=====>>> Executing @Before advice on everything)\n");
+
+        // display the method signature
+        System.out.println("Method: " + theJoinPoint.getSignature().toShortString());
+
+        // display the method arguments
+
+        Object[] args = theJoinPoint.getArgs();
+        for (Object tempArg : args) {
+            System.out.println(tempArg);
+
+            if (tempArg instanceof com.luv2code.aopdemo.Account) {
+                com.luv2code.aopdemo.Account theAccount = (com.luv2code.aopdemo.Account) tempArg;
+                System.out.println("account name: " + theAccount.getName());
+                System.out.println("account level: " + theAccount.getLevel());
+            }
+        }
+
     }
 }
